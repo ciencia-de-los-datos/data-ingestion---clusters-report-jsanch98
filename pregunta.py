3ca690318f990d04b7b5014e719bbf80c73120f3
Joshua Sanchez <joshua.sanchez@qiip.com.co>
2023-03-17 17:59:57 -0500

    Solucion lab data ingestion

--- pregunta.py
+++ pregunta.py
@@ -17,5 +17,20 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
+    rangos_columnas = [(0, 9), (9, 25), (25, 41), (41, 118)]
+    nombres_columnas = pd.read_fwf('clusters_report.txt',
+                                    nrows=2,
+                                    header=None,
+                                    colspecs=rangos_columnas).fillna('').apply(lambda x: ' '.join(x).strip().replace(' ', '_').lower()).values
+    df = pd.read_fwf('clusters_report.txt',
+                      skiprows=4, 
+                      names=nombres_columnas
+                      )
 
+    df['cluster'] = df['cluster'].ffill()
+    df['principales_palabras_clave'] = df[['cluster','principales_palabras_clave']].groupby(['cluster'])['principales_palabras_clave'].transform(lambda linea_datos: ' '.join(linea_datos).replace('.',''))
+    df = df.dropna()
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].transform(lambda porcentaje: porcentaje.replace(' %','').replace(',','.').strip())
+    df = df.astype({"cluster": 'Int16', "cantidad_de_palabras_clave": 'Int16', 'porcentaje_de_palabras_clave': 'Float64', 'principales_palabras_clave':'string'})
+    df = df.replace(r'\s+', ' ', regex=True)
     return df
